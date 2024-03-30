b173174af32208188bb6bac7a8780c85ae5abb47
Yousef <gx6gx6gx@gmail.com>
2024-03-30 00:08:10 -0400

    Final

--- src/components/Windows/Main.js
+++ src/components/Windows/Main.js
@@ -3,7 +3,8 @@ import { Rnd } from 'react-rnd';
 import QuestionMode1 from './MainWindow/QuestionMode1';
 import QuestionMode2 from './MainWindow/QuestionMode2';
 import { FaStar, FaToggleOff, FaToggleOn } from 'react-icons/fa';
-
+import { FaPlus, FaEdit, FaTrash, FaBook, FaDoorOpen } from 'react-icons/fa';
+import App from '../../App';
 const questions = [
   {
     question: 'What is the capital of France?',
@@ -34,7 +35,6 @@ function Main({ id, onClose, zIndex, bringToFront }) {
   const [rating, setRating] = useState(0);
   const [questionMode, setQuestionMode] = useState('mode1');
 
-
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
@@ -63,7 +63,7 @@ function Main({ id, onClose, zIndex, bringToFront }) {
       onMouseDown={bringToFront}
       enableResizing={false}
     >
-     <div className="flex-none bg-indigo-700 flex items-center justify-between relative">
+      <div className="flex-none bg-indigo-700 flex items-center justify-between relative">
         <span className="text-white text-lg font-semibold ml-4"> Quiz Window</span>
         <div className="mr-2 flex flex-col items-end">
           <button
@@ -86,70 +86,127 @@ function Main({ id, onClose, zIndex, bringToFront }) {
           </button>
         </div>
       </div>
-      <div className="flex-grow flex">
-        {/* Sidebar */}
+      {/* Sidebar */}
+<div className="flex-grow flex">
+  <div
+    className={`${
+      sidebarOpen ? 'w-fit' : 'w-16'
+    } bg-gray-100 p-4 transition-width duration-300 flex flex-col justify-between`}
+  >
+    <div className="flex flex-col items-center">
+      <button
+        // onClick={toggleSidebar}
+        className="text-gray-600 hover:text-gray-800 focus:outline-none mb-10"
+      >
+        {/* {sidebarOpen ? '⬅️' : '➡️'} */}
+
+        <img src={require('./logoR.png')} height={10} width={40}/>
+      </button>
+      <div className="flex flex-col items-center space-y-8 mb-4">
         <div
-          className={`${
-            sidebarOpen ? 'w-1/4' : 'w-8'
-          } bg-gray-100 p-4 transition-width duration-300 overflow-hidden`}
+          className="sidebar-link relative cursor-pointer"
+          onMouseEnter={(e) => e.currentTarget.querySelector('span').classList.remove('hidden')}
+          onMouseLeave={(e) => e.currentTarget.querySelector('span').classList.add('hidden')}
         >
-          <button
-            onClick={toggleSidebar}
-            className="text-gray-600 hover:text-gray-800 focus:outline-none"
-          >
-            {sidebarOpen ? '⬅️' : '➡️'}
-          </button>
-          {sidebarOpen && (
-            <p className="mt-4 text-gray-800 font-medium">Sidebar Content</p>
-          )}
+          <div className="flex items-center bg-white rounded-full p-2 shadow-md">
+            <FaPlus className="text-green-400" />
+          </div>
+          <span className="hidden absolute bottom-0 translate-y-full bg-gray-700 text-white text-xs px-2 py-1 rounded-md">
+            Add Card
+          </span>
         </div>
-        {/* Main content area */}
+        <div
+          className="sidebar-link relative cursor-pointer"
+          onMouseEnter={(e) => e.currentTarget.querySelector('span').classList.remove('hidden')}
+          onMouseLeave={(e) => e.currentTarget.querySelector('span').classList.add('hidden')}
+        >
+          <div className="flex items-center bg-white rounded-full p-2 shadow-md">
+            <FaEdit className="text-yellow-500" />
+          </div>
+          <span className="hidden absolute bottom-0 translate-y-full bg-gray-700 text-white text-xs px-2 py-1 rounded-md">
+            Edit Card
+          </span>
+        </div>
+        <div
+          className="sidebar-link relative cursor-pointer"
+          onMouseEnter={(e) => e.currentTarget.querySelector('span').classList.remove('hidden')}
+          onMouseLeave={(e) => e.currentTarget.querySelector('span').classList.add('hidden')}
+        >
+          <div className="flex items-center bg-white rounded-full p-2 shadow-md">
+            <FaTrash className="text-red-500" />
+          </div>
+          <span className="hidden w-fit absolute bottom-0 translate-y-full bg-gray-700 text-white text-xs px-3 py-1 rounded-md">
+            Delete Card
+          </span>
+        </div>
+        <div
+          className="sidebar-link relative cursor-pointer"
+          onMouseEnter={(e) => e.currentTarget.querySelector('span').classList.remove('hidden')}
+          onMouseLeave={(e) => e.currentTarget.querySelector('span').classList.add('hidden')}
+        >
+          <div className="flex items-center bg-white rounded-full p-2 shadow-md">
+            <FaBook className="text-blue-500" />
+          </div>
+          <span className="hidden absolute bottom-0 translate-y-full bg-gray-700 text-white text-xs px-2 py-1 rounded-md">
+            Browse Cards
+          </span>
+        </div>
+      </div>
+    </div>
+    <div className="cursor-pointer sidebar-link flex items-center px-2 py-1 hover:bg-gray-200 rounded-md transition-colors duration-300">
+      <div className="mr-4 bg-white rounded-full p-1">
+        <FaDoorOpen className="text-black-500" />
+      </div>
+      <span className="text-gray-700 font-semibold">Exit</span>
+    </div>
+  </div>
+  {/* Main content area */}
         <div
           className={`${
             sidebarOpen ? 'w-3/4' : 'w-full'
           } p-4 flex flex-col bg-gray-50`}
         >
-    {questionMode === 'mode1' ? (
-          <QuestionMode1
-            currentQuestion={currentQuestion}
-            questions={questions}
-            showAnswer={showAnswer}
-            handleShowAnswer={handleShowAnswer}
-            handleNextQuestion={handleNextQuestion}
-          />
-        ) : (
-          <QuestionMode2
-            currentQuestion={currentQuestion}
-            questions={questions}
-            handleShowAnswer={handleShowAnswer}
-            handleNextQuestion={handleNextQuestion}
-          />
-        )}
-         {/* Rate Preferomance Bar */}
-         {showAnswer && (
-        <div className="mt-4">
-          <p className="text-sm font-semibold mb-2 text-gray-700">
-            Rate your performance:
-          </p>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            {[1, 2, 3, 4, 5].map((value) => (
-              <FaStar
-                key={value}
-                color={value <= rating ? '#ffc107' : '#e2e8f0'}
-                size={24}
-                className="cursor-pointer"
-                onClick={() => handleRating(value)}
-              />
-            ))}
-          </div>
-        </div>
-      )}
+          {questionMode === 'mode1' ? (
+            <QuestionMode1
+              currentQuestion={currentQuestion}
+              questions={questions}
+              showAnswer={showAnswer}
+              handleShowAnswer={handleShowAnswer}
+              handleNextQuestion={handleNextQuestion}
+            />
+          ) : (
+            <QuestionMode2
+              currentQuestion={currentQuestion}
+              questions={questions}
+              handleShowAnswer={handleShowAnswer}
+              handleNextQuestion={handleNextQuestion}
+            />
+          )}
+          {/* Rate Preferomance Bar */}
+          {showAnswer && (
+            <div className="mt-4">
+              <p className="text-sm font-semibold mb-2 text-gray-700">
+                Rate your performance:
+              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                {[1, 2, 3, 4, 5].map((value) => (
+                  <FaStar
+                    key={value}
+                    color={value <= rating ? '#ffc107' : '#e2e8f0'}
+                    size={24}
+                    className="cursor-pointer"
+                    onClick={() => handleRating(value)}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
           {/* Progress Bar */}
           <div className="h-3 bg-gray-300 rounded-full mt-4 overflow-hidden">
             <div
