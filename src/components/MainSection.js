b640f71b2e7dabeb6e7cbfdb87ec0aae04359644
Kohei Yasui <kyasui@uwo.ca>
2024-03-30 21:00:35 -0400

    merge

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,12 +1,10 @@
-
 import React, { useState } from "react";
-import Main from "./Windows/MainWindow/Main";
+import Main from "./Windows/Main";
 import AddCards from "./Windows/addCardWindows/AddCards";
 import BrowseCards from "./Windows/BrowseCards";
 import EditCard from "./Windows/EditCard";
 import ExampleMulWindow from "./Windows/ExampleMulWindow";
 import Statistics from "./Windows/Statistics";
-
 
 function MainSection() {
   const [activeWindows, setActiveWindows] = useState(Array(6).fill(false));
